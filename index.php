27e75ef0225ea50db15008a500d0dffc63f14722
lucasfernando9 <sortudodois@hotmail.com>
2018-06-13 19:30:24 -0300

    Testando do meu computador

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+        echo "<strong>Oi</strong> <h1>mundo</h1>!"
+    ?>
+</div>
+</body>
+
+</html>
